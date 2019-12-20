0dd09a544ddaee4d4e4b0e2e7ef239d34edb908f
ZacharyEilers <zacharyeilers03@gmail.com>
2019-12-20 17:32:53 -0600

    update showcase, add shadows to cards, update navbar

--- scripts.js
+++ scripts.js
@@ -4,6 +4,14 @@ const scrollThreshold = 50;
 
 const navbar = document.querySelector("#navbar");
 
+const stylesAtTop = {
+  boxShadow: "0px 0px 0px 0px rgba(0, 0, 0, 0)"
+};
+
+const stylesNotAtTop = {
+  boxShadow: "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)"
+};
+
 window.addEventListener("scroll", event => setNavbarBackground(windowScrollY));
 
 function setNavbarBackground(windowScroll) {
@@ -11,12 +19,12 @@ function setNavbarBackground(windowScroll) {
     this.window.scrollY > scrollThreshold &&
     windowScroll <= scrollThreshold
   ) {
-    navbar.style.backgroundColor = "rgba(0, 0 , 0, 0.8)";
+    navbar.style.boxShadow = stylesNotAtTop.boxShadow;
   } else if (
     this.window.scrollY < scrollThreshold &&
     windowScroll >= scrollThreshold
   ) {
-    navbar.style.backgroundColor = "rgba(0, 0 , 0, 0.2)";
+    navbar.style.boxShadow = stylesAtTop.boxShadow;
   }
   windowScrollY = this.window.scrollY;
 }
